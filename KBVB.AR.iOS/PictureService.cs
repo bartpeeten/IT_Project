65df289311f0adaeb48fe63a96b62ed6ef8247dc
bartpeeten <bart.peeten@icloud.com>
2018-11-12 21:39:49 +0100

    KBVB.AR.iOS

---
+++ KBVB.AR.iOS/PictureService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Foundation;
+using KBVB.AR.Services;
+using UIKit;
+
+namespace KBVB.AR.iOS
+{
+    //Permission IOS
+    //<key>NSPhotoLibraryUsageDescription</key>
+    //<string>Picture Service uses photo library</string>
+    public class PictureService : IPictureService
+    {
+        TaskCompletionSource<Stream> taskCompletionSource;
+        UIImagePickerController imagePicker;
+
+        public Task<Stream> GetImageStreamAsync()
+        {
+            // Create and define UIImagePickerController
+            imagePicker = new UIImagePickerController
+            {
+                SourceType = UIImagePickerControllerSourceType.PhotoLibrary,
+                MediaTypes = UIImagePickerController.AvailableMediaTypes(UIImagePickerControllerSourceType.PhotoLibrary)
+            };
+
+            // Set event handlers
+            imagePicker.FinishedPickingMedia += OnImagePickerFinishedPickingMedia;
+            imagePicker.Canceled += OnImagePickerCancelled;
+
+            // Present UIImagePickerController;
+            var window = UIApplication.SharedApplication.KeyWindow;
+            var viewController = window.RootViewController;
+            viewController.PresentModalViewController(imagePicker, true);
+
+            // Return Task object
+            taskCompletionSource = new TaskCompletionSource<Stream>();
+            return taskCompletionSource.Task;
+        }
+
+        private void OnImagePickerFinishedPickingMedia(object sender, UIImagePickerMediaPickedEventArgs args)
+        {
+            UIImage image = args.EditedImage ?? args.OriginalImage;
+
+            if (image != null)
+            {
+                // Convert UIImage to .NET Stream object
+                NSData data = image.AsJPEG(1);
+                Stream stream = data.AsStream();
+
+                // Set the Stream as the completion of the Task
+                taskCompletionSource.SetResult(stream);
+            }
+            else
+            {
+                taskCompletionSource.SetResult(null);
+            }
+            imagePicker.DismissModalViewController(true);
+        }
+
+        private void OnImagePickerCancelled(object sender, EventArgs args)
+        {
+            taskCompletionSource.SetResult(null);
+            imagePicker.DismissModalViewController(true);
+        }
+    }
+}
---
+++ KBVB.AR.iOS/Services/ShareService.cs
@@ -0,0 +1,35 @@
+﻿using Foundation;
+using KBVB.AR.iOS.Services;
+using KBVB.AR.Services;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: Xamarin.Forms.Dependency(typeof(ShareService))]
+namespace KBVB.AR.iOS.Services
+{
+    public class ShareService : IShareService
+    {
+        public async void Share(string subject, string message, ImageSource imageSource)
+        {
+            var handler = new ImageLoaderSourceHandler();
+            var uiImage = await handler.LoadImageAsync(imageSource);
+            var img = NSObject.FromObject(uiImage);
+            var msg = NSObject.FromObject(message);
+            var activityItems = new[] { msg, img };
+            var activityController = new UIActivityViewController(activityItems, null);
+            var topController = UIApplication.SharedApplication.KeyWindow.RootViewController;
+
+            while (topController.PresentedViewController != null)
+            {
+                topController = topController.PresentedViewController;
+            }
+
+            topController.PresentViewController(activityController, true, () => { });
+        }
+    }
+}
