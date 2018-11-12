15b32550dea1b2d1d68e79e52cdc9aba43c2f761
bartpeeten <bart.peeten@icloud.com>
2018-11-12 21:38:32 +0100

    KBVB.AR.Android

---
+++ KBVB.AR.Android/PictureServiceActivity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace KBVB.AR.Droid
+{
+    [Activity(Label = "PictureServiceActivity")]
+    public class PictureServiceActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        public static readonly int PickImageId = 1000;
+
+        public TaskCompletionSource<Stream> PickImageTaskCompletionSource { set; get; }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent intent)
+        {
+            base.OnActivityResult(requestCode, resultCode, intent);
+
+            if (requestCode == PickImageId)
+            {
+                if ((resultCode == Result.Ok) && (intent != null))
+                {
+                    Android.Net.Uri uri = intent.Data;
+                    Stream stream = ContentResolver.OpenInputStream(uri);
+
+                    // Set the Stream as the completion of the Task
+                    PickImageTaskCompletionSource.SetResult(stream);
+                }
+                else
+                {
+                    PickImageTaskCompletionSource.SetResult(null);
+                }
+            }
+        }
+    }
+}
---
+++ KBVB.AR.Android/Services/ShareService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using KBVB.AR.Droid.Services;
+using KBVB.AR.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly:Dependency(typeof(ShareService))]
+namespace KBVB.AR.Droid.Services
+{
+    public class ShareService : Activity, IShareService
+    {
+        public async void Share(string subject, string message, ImageSource imageSource)
+        {
+            var intent = new Intent(Intent.ActionSend);
+            //intent.PutExtra(Intent.ExtraSubject, subject);
+            intent.PutExtra(Intent.ExtraText, message);
+            intent.SetType("image/png");
+
+            var handler = new ImageLoaderSourceHandler();
+            var bitmap = await handler.LoadImageAsync(imageSource, this);
+
+            var path = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures +
+                Java.IO.File.Separator + "player.png");
+
+            using (var os = new System.IO.FileStream(path.AbsolutePath, System.IO.FileMode.Create))
+            {
+                bitmap.Compress(Bitmap.CompressFormat.Png, 100, os);
+            }
+
+            intent.PutExtra(Intent.ExtraStream, Android.Net.Uri.FromFile(path));
+            MainApplication.CurrentContext.StartActivity(Intent.CreateChooser(intent, "Share Image"));
+        }
+    }
+}
